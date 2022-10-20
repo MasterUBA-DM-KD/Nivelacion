b24127bf36f52fda577fd0576aaf9b6ce6d672cb
UribeAlejandro <jauriberamirez@gmail.com>
2022-10-19 22:06:58 -0300

    Up to cuadratica

--- 1_Cuatrimestre/1_Algoritmos/README.md
+++ 1_Cuatrimestre/1_Algoritmos/README.md
@@ -2,6 +2,8 @@
 # Tabla de contenido <!-- omit in toc -->
 - [Notebooks](#notebooks)
 - [Examen](#examen)
+  - [Prototipo](#prototipo)
+  - [Actual](#actual)
 # Notebooks
 - [Introducción](notebooks/P1_Nivelación_algoritmos_(UBA_2022).ipynb)
   - [Cheat Sheet - Colab](otros/Cheat_sheet_for_Google_Colab.ipynb)
@@ -11,5 +13,8 @@
 - [Programación Orientada a Objetos](notebooks/P5_Nivelación_Algoritmos_(UBA_2022).ipynb)
   - [Herencia múltiple](otros/Ejemplo_herencia_y_herencia_multiple.ipynb)
 # Examen
+## Prototipo
 - [Prototipo de examen](otros/Algoritmos_Examen_2020.pdf)
-- [Solución examen]oOtros/SolucionExamen.ipynb)
+- [Solución examen](Otros/SolucionExamen.ipynb)
+## Actual
+- [Solución](otros/Examen.md)
--- 1_Cuatrimestre/4_Elem_Mat_Comp_para_Estadistica/2_Programacion/Clase_1/grupal/1_cuadratica.R
+++ 1_Cuatrimestre/4_Elem_Mat_Comp_para_Estadistica/2_Programacion/Clase_1/grupal/1_cuadratica.R
@@ -1,21 +1,68 @@
-ceros_cuadratica <- function(a, b, c){
-  delta <- b^2 - 4*a*c
-  if (ddelta >= 0) {
-    root_1 = (-b - sqrt(delta)) / 2*a
-    root_2 = (-b + sqrt(delta)) / 2*a
-    raices = (root_1, root_2)
+# Escriba una función que reciba los coeficientes del polinomio
+# y devuelva un vector con las posibles raíces del polinomios.
+# Use el siguiente modelo como base:
+
+ceros_cuadratica <- function(a, b, c) {
+  delta <- b^2 - 4 * a * c
+  if (delta >= 0) {
+    root_1 <- (-b - sqrt(delta)) / 2 * a
+    root_2 <- (-b + sqrt(delta)) / 2 * a
+    raices <- c(root_1, root_2)
   } else {
-    raices = "No roots"
+    raices <- "No roots"
   }
   return(raices)
 }
+# En lugar de ingresar los coeficientes por separado, repita el ejercicio
+# pero ingrese un vector de coeficientes.
+# Use el siguiente modelo como base:
 
-ceros_cuadratica = function(a, b, c){ discriminante \<- if () return(raices) }
+ceros_cuadratica_vector <- function(coef_vector) {
+  if (length(coef_vector) == 3) {
+    a <- coef_vector[1]
+    b <- coef_vector[2]
+    c <- coef_vector[3]
+    delta <- b^2 - 4 * a * c
 
-decidir_raiz = function(fun, valor){ 
-  es_raiz = ...
+    if (delta >= 0) {
+      root_1 <- (-b - sqrt(delta)) / 2 * a
+      root_2 <- (-b + sqrt(delta)) / 2 * a
+      raices <- c(root_1, root_2)
+    } else {
+      raices <- "No roots"
+    }
+    return(raices)
+  } else {
+    return("Invalid input")
+  }
+}
+
+# Escriba una función que reciba una función matemática y un valor
+# y devuelva si dicho valor es raíz de la función.
+# Use el siguiente modelo como base:
+
+decidir_raiz <- function(fun, x) {
+  es_raiz <- eval(parse(text = fun)) == 0
   return(es_raiz)
 }
-  
-graficar_funcion_y\_valor = function(fun, valor){ \# ... }
-    
+
+decidir_raiz("2*x^2", 0)
+
+# Escriba una función que grafique una función junto con un valor dado.
+# Que diga en el título del gráfico si es o no raíz de la función.
+graficar_funcion_y_valor <- function(fun, valor) {
+  x <- valor
+  y_val <- eval(parse(text = fun))
+  x_0 <- valor - 10
+  x_n <- valor + 10
+
+  x <- x_0:x_n
+  y <- eval(parse(text = fun))
+
+  plot(x, y, col = "red", type = "l", xlab = "x", ylab = "f(x)")
+  points(valor, y_val, col = "blue")
+
+  legend(1, 95, legend = c(fun, paste("(", valor, ",", y_val, ")", sep = "")), col = c("red", "blue"), lty = 1:2, cex = 0.8, border = "white")
+}
+
+graficar_funcion_y_valor("x^2", 0)
